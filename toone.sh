bf11ff1da86f01e99f9d649bff19e0d0cb57d74a
twofacefil11 <twofacefil11@gmail.com>
2025-05-30 16:28:44 +0200

    another small cleanup

--- toone.sh
+++ toone.sh
@@ -2,10 +2,8 @@
 
 OUTPUT_FILE="combined_files.txt"
 
-# Clear or create the output file
 > "$OUTPUT_FILE"
 
-# Function to process files in a directory
 process_directory() {
     local dir=$1
     local extension=$2
@@ -17,15 +15,13 @@ process_directory() {
     done
 }
 
-# Process .h and .hpp files in include/
 if [ -d "include" ]; then
     process_directory "include" "h"
     process_directory "include" "hpp"
 fi
 
-# Process .cpp files in src/
 if [ -d "src" ]; then
     process_directory "src" "c"
 fi
 
-echo "All files have been combined into $OUTPUT_FILE"
+echo "All combined to: $OUTPUT_FILE"
