8e5332fb0278755b4898e107c59f6a7733dbee7a
twofacefil11 <twofacefil11@gmail.com>
2025-06-12 15:51:08 +0200

    fix: clang settings

--- src/main.c
+++ src/main.c
@@ -25,34 +25,45 @@
 
 // ----------------------------------------------------------------
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine,
-                   int nCmdShow) {
+int WINAPI
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        PSTR pCmdLine,
+        int nCmdShow) {
 
-  State *appState = init_state(hInstance);
-  if (!appState)
-    return -1; // ERR
+    State* appState = init_state(hInstance);
+    if (!appState)
+        return -1; // ERR
 
-  register_class(appState, hInstance, MAIN_WINDOW_CLASS_NAME);
+    register_class(appState, hInstance, MAIN_WINDOW_CLASS_NAME);
 
-  Window_initial_position wip = getWindowInfo();
+    Window_initial_position wip = getWindowInfo();
 
-  appState->UI_handles.hwnd_main = CreateWindowW(
-      MAIN_WINDOW_CLASS_NAME, (LPCWSTR)("FilterLAB"),
-      WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CLIPCHILDREN, wip.x, wip.y,
-      wip.width, wip.height, NULL, NULL, hInstance, appState);
+    appState->UI_handles.hwnd_main =
+      CreateWindowW(MAIN_WINDOW_CLASS_NAME,
+                    (LPCWSTR)("FilterLAB"),
+                    WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CLIPCHILDREN,
+                    wip.x,
+                    wip.y,
+                    wip.width,
+                    wip.height,
+                    NULL,
+                    NULL,
+                    hInstance,
+                    appState);
 
-  if (!appState->UI_handles.hwnd_main) {
-    return -1; // ERR
-  }
+    if (!appState->UI_handles.hwnd_main) {
+        return -1; // ERR
+    }
 
-  // ----------------------------------------------------------------
+    // ----------------------------------------------------------------
 
-  MSG message;
-  while (GetMessage(&message, NULL, 0, 0)) {
-    TranslateMessage(&message);
-    DispatchMessage(&message);
-  }
+    MSG message;
+    while (GetMessage(&message, NULL, 0, 0)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
 
-  // ----------------------------------------------------------------
-  return 0;
+    // ----------------------------------------------------------------
+    return 0;
 }
